82d9a0c08661b9d5aca6a450a66b38df8d0a521a
Stepa Tech <olegvoronin953@gmail.com>
2025-05-06 22:36:24 +0500

    тест коммит

--- lib/models/exercise_parsing.dart
+++ lib/models/exercise_parsing.dart
@@ -1,3 +1,48 @@
+class Exercise {
+  final String id;
+  final String name;
+  final int sets;
+  final int reps;
+  final List<ProgressEntry> progress;
+
+  Exercise({
+    required this.id,
+    required this.name,
+    required this.sets,
+    required this.reps,
+    required this.progress,
+  });
+
+  factory Exercise.fromJson(Map<String, dynamic> json) {
+    return Exercise(
+      id: json['id'],
+      name: json['name'],
+      sets:
+          json['sets'] is int
+              ? json['sets']
+              : int.parse(json['sets'].toString()),
+      reps:
+          json['reps'] is int
+              ? json['reps']
+              : int.parse(json['reps'].toString()),
+      progress:
+          (json['progress'] as List<dynamic>)
+              .map((entry) => ProgressEntry.fromJson(entry))
+              .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'sets': sets,
+      'reps': reps,
+      'progress': progress.map((e) => e.toJson()).toList(),
+    };
+  }
+}
+
 class ProgressEntry {
   final String date;
   final double weight;
@@ -7,47 +52,14 @@ class ProgressEntry {
   factory ProgressEntry.fromJson(Map<String, dynamic> json) {
     return ProgressEntry(
       date: json['date'],
-      weight: (json['weight'] as num).toDouble(),
+      weight:
+          json['weight'] is double
+              ? json['weight']
+              : double.parse(json['weight'].toString()),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {'date': date, 'weight': weight};
-  }
-}
-
-class Exercise {
-  final String name;
-  final int sets;
-  final int reps;
-  final List<ProgressEntry> progress;
-
-  Exercise({
-    required this.name,
-    required this.sets,
-    required this.reps,
-    required this.progress,
-  });
-
-  factory Exercise.fromJson(Map<String, dynamic> json) {
-    var progressList = json['progress'] as List;
-    List<ProgressEntry> parsedProgress =
-        progressList.map((e) => ProgressEntry.fromJson(e)).toList();
-
-    return Exercise(
-      name: json['name'],
-      sets: json['sets'],
-      reps: json['reps'],
-      progress: parsedProgress,
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'sets': sets,
-      'reps': reps,
-      'progress': progress.map((e) => e.toJson()).toList(),
-    };
   }
 }
