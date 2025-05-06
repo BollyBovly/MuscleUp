43b933140c6cf5c3c63fe87cbb8d1520106ab7b7
Stepa Tech <olegvoronin953@gmail.com>
2025-05-06 19:33:57 +0500

    елки палки оно должно работать 2 я через гпт ее делал)0)

---
+++ lib/models/exercise.dart
@@ -0,0 +1,54 @@
+class Exercise {
+  String name;
+  int sets;
+  int reps;
+  List<ProgressEntry> progress;
+
+  Exercise({
+    required this.name,
+    required this.sets,
+    required this.reps,
+    required this.progress,
+  });
+
+  factory Exercise.fromJson(Map<String, dynamic> json) {
+    return Exercise(
+      name: json['name'],
+      sets: json['sets'],
+      reps: json['reps'],
+      progress: (json['progress'] as List<dynamic>)
+          .map((item) => ProgressEntry.fromJson(item))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'sets': sets,
+      'reps': reps,
+      'progress': progress.map((e) => e.toJson()).toList(),
+    };
+  }
+}
+
+class ProgressEntry {
+  String date;
+  double weight;
+
+  ProgressEntry({required this.date, required this.weight});
+
+  factory ProgressEntry.fromJson(Map<String, dynamic> json) {
+    return ProgressEntry(
+      date: json['date'],
+      weight: (json['weight'] as num).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'date': date,
+      'weight': weight,
+    };
+  }
+}
---
+++ lib/services/local_data_service.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import '../models/exercise.dart';
+
+class LocalDataService {
+  static const _fileName = 'exercises.json';
+
+  Future<File> _getLocalFile() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return File('${dir.path}/$_fileName');
+  }
+
+  Future<void> _copyFromAssetsIfNeeded() async {
+    final file = await _getLocalFile();
+    if (!(await file.exists())) {
+      final jsonString = await rootBundle.loadString(
+        'assets/data/exercises.json',
+      );
+      await file.writeAsString(jsonString);
+    }
+  }
+
+  Future<List<Exercise>> loadExercises() async {
+    await _copyFromAssetsIfNeeded();
+    final file = await _getLocalFile();
+    final content = await file.readAsString();
+    final jsonData = jsonDecode(content) as List;
+    return jsonData.map((e) => Exercise.fromJson(e)).toList();
+  }
+
+  Future<void> saveExercises(List<Exercise> exercises) async {
+    final file = await _getLocalFile();
+    final jsonString = jsonEncode(exercises.map((e) => e.toJson()).toList());
+    await file.writeAsString(jsonString);
+  }
+
+  Future<void> addProgressToExercise(int index, double newWeight) async {
+    final exercises = await loadExercises();
+    final today = DateTime.now();
+    final dateStr =
+        "${today.day.toString().padLeft(2, '0')}-"
+        "${today.month.toString().padLeft(2, '0')}-"
+        "${today.year}";
+
+    exercises[index].progress.add(
+      ProgressEntry(date: dateStr, weight: newWeight),
+    );
+
+    await saveExercises(exercises);
+  }
+}
